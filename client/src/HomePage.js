f72a63000d91aa262e1a919ccc5603df6a1edb0a
macbook air <macbookair@macbooks-Air.home>
2023-06-24 11:44:08 -0700

    added homepage and tracking routers

---
+++ client/src/HomePage.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './HomePage.css';
+
+function HomePage() {
+    console.log("Homepage rendered");
+  return (
+<h1>HomePage</h1>
+  );
+}
+
+export default HomePage;
---
+++ client/src/TrackingPage.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './TrackingPage.css';
+
+function TrackingPage() {
+  return (
+<h1>Tracking Page</h1>
+  );
+}
+
+export default TrackingPage;
